fee6c9375d508e7b5cdd966d8cdad85864987cb6
Gerald Rush <gtrush4@gmail.com>
2025-09-17 01:26:14 -0700

    Remove react template and insert book club image

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -2,7 +2,7 @@ import type {BookProps} from "../data/books.ts";
 
 export default function Book(props: BookProps) {
   return (
-      <div style={{display: 'flex', flexDirection: 'column'}}>
+      <div style={{textAlign: 'center', display: 'flex', flexDirection: 'column'}}>
         <h1>{props.title}</h1>
         <p>{props.description}</p>
       </div>
